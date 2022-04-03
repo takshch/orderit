db444aa6c221b9cdda1da3219cb91b2a79acf14b
takshch <takshch089@gmail.com>
2022-04-04 05:02:22 +0530

    Adds axios

--- client/src/services/products.js
+++ client/src/services/products.js
@@ -1,11 +1,12 @@
+import axios from "axios";
+
 const API = 'http://localhost:4000';
 const PRODUCTS_API = `${API}/products`;
 
 class ProductService {
   async loadAll() {
-    const response = await fetch(PRODUCTS_API);
-    const data = await response.json();
-    return data;
+    const response = await axios.get(PRODUCTS_API);
+    return response.data;
   }
 }
 
