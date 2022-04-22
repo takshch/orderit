af7932bebbfde4e3e4c86f3c784978f790da0f5d
takshch <takshch089@gmail.com>
2022-04-22 17:21:18 +0530

    Simplify createAccount function

--- backend/src/controllers/session.ts
+++ backend/src/controllers/session.ts
@@ -7,21 +7,22 @@ const createAccount: RequestHandler = async (req, res) => {
   try {
     const uid = await AuthService.createUser(req.body);
 
-    if (uid) {
-      const token = generateAccessToken({ uid });
-
-      const cookieParams = {
-        httpOnly: true,
-        sameSite: true,
-        expires: new Date(Date.now() + (24 * 60 * 60 * 1000)),
-        signed: true,
-      };
-      res.cookie(config.get('cookieName'), token, cookieParams);
-
-      res.status(201).send({ uid });
-    } else {
+    if (!uid) {
       res.status(200).send({ message: 'user already exists' });
+      return;
     }
+
+    const token = generateAccessToken({ uid });
+
+    const cookieParams = {
+      httpOnly: true,
+      sameSite: true,
+      expires: new Date(Date.now() + (24 * 60 * 60 * 1000)),
+      signed: true,
+    };
+    res.cookie(config.get('cookieName'), token, cookieParams);
+
+    res.status(201).send({ uid });
   } catch (e) {
     console.log(e);
     res.boom.badImplementation();
