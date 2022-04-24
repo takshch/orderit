acaef762c94efbb9a72120750cd7946220b9f3d4
takshch <takshch089@gmail.com>
2022-04-25 00:03:43 +0530

    Enables cors

--- backend/src/app-initializer.ts
+++ backend/src/app-initializer.ts
@@ -4,10 +4,20 @@ import morgan from 'morgan';
 import { MorganStream } from './utils/logger';
 import cookieParser from 'cookie-parser';
 import fs from 'fs';
+import cors from 'cors';
+import config from 'config';
+
+const { assign } = Object;
 
 const cookieSECRET = fs.readFileSync('keys/cookie.key', 'utf8');
 if (!cookieSECRET) {
   throw new Error('cookie key must exists');
+}
+
+const corsOptions = { credentials: true };
+{
+  const origin = config.get('cors_origin');
+  assign(corsOptions, { origin });
 }
 
 function AppInitializer(app: Express) {
@@ -16,6 +26,8 @@ function AppInitializer(app: Express) {
   app.use(boom());
 
   app.use(morgan('combined', { stream: new MorganStream() }));
+
+  app.use(cors(corsOptions));
 
   // only parse response if Content type matches
   app.use(express.json());
