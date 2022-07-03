99e5420e189339b9bd89abaece4ae960b92d6eb7
takshch <takshch089@gmail.com>
2022-07-03 16:03:32 +0530

    Adds scan QR and redirect to shop page

--- client/src/route/customer/index.js
+++ client/src/route/customer/index.js
@@ -1,18 +1,88 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import Products from '../../components/Products';
-import AddToCart from '../../components/AddToCart';
+import React, { useEffect, useRef, useState } from 'react';
+import jsQR from 'jsqr';
 import './index.scss';
+import { useNavigate } from 'react-router-dom';
+
+function isURL(string) {
+  try {
+    new URL(string);
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+}
 
 function RouteCustomerIndex() {
-  const { isLoading } = useSelector((state) => state.productsIds);
+  const canvasRef = useRef(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [data, setData] = useState(null);
+  const navigate = useNavigate();
 
-  return <div className="route-customer">
-    {
-      isLoading ? "Loading . . . " : <Products />
+  useEffect(() => {
+    const video = document.createElement('video');
+    let frameRequest;
+
+    const update = () => {
+      const { current: canvas } = canvasRef;
+      const ctx = canvas.getContext('2d');
+
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+      const code = jsQR(imageData.data, canvas.width, canvas.height);
+      if (code) {
+        setData(code.data);
+      }
+
+      frameRequest = requestAnimationFrame(update);
+    };
+
+    const handleStream = (stream) => {
+      setIsRecording(true);
+
+      video.srcObject = stream;
+      video.play();
+      update();
+    };
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(handleStream)
+        .catch((err) => {
+          console.log('error', err);
+          setIsRecording(false);
+        });
     }
-    <AddToCart />
-  </div>;
+
+    return () => {
+      video.remove();
+      cancelAnimationFrame(frameRequest);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (data && isURL(data)) {
+      const url = new URL(data);
+      const pathnames = url.pathname.split('/');
+      if (pathnames.length === 3) {
+        navigate(url.pathname);
+      }
+    }
+  }, [data, navigate]);
+
+  return (
+    <div className="route-customer">
+      {
+        !isRecording && "Please allow the camera permission to scan the QR Code"
+      }
+
+      <div className="scanner">
+        <div className="square"></div>
+        <canvas ref={canvasRef} />
+      </div>
+    </div>
+  );
 };
 
 export default RouteCustomerIndex;
